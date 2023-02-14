b1932325a02e789b27da93f56f6f63965a5c4058
Aarya Bhatia <aarya.bhatia1678@gmail.com>
2023-02-14 02:20:53 +0000

    Bug fixes and refactoring

--- src/epoll_client.c
+++ src/epoll_client.c
@@ -12,6 +12,7 @@
 #define PING_INTERVAL_SEC 3
 #define EPOLL_TIMEOUT 1000
 #define MAX_QUEUE_SIZE 100
+#define MAX_EPOLL_EVENTS 3 // stdin, socket, timer
 
 static char *g_line = NULL;
 static size_t g_line_len = 0;
@@ -22,6 +23,181 @@ static volatile bool running = true;
 
 // message queue to store outgoing messages
 static CC_List *g_queue = NULL;
+
+void read_user_input();
+void signal_handler(int sig);
+void cb(void *msg);
+void quit();
+void read_from_socket(int sock, char read_buf[], size_t *read_buf_len);
+void write_to_socket(int sock, char write_buf[], size_t *write_buf_len, size_t *write_buf_off);
+int create_timer(int duration_sec);
+
+int main(int argc, char *argv[])
+{
+    if (argc != 3)
+    {
+        fprintf(stderr, "Usage: %s <hostname> <port>\n", *argv);
+        return 1;
+    }
+
+    Client_init(&g_client);
+    Client_connect(&g_client, argv[1], argv[2]);
+
+    cc_list_new(&g_queue);
+
+    g_last_request = time(NULL);
+
+    // Create fd for epoll
+    int epoll_fd = epoll_create1(0);
+    CHECK(epoll_fd, "epoll_create1");
+
+    // Create a timer fd to send PING messages
+    int timerfd = create_timer(PING_INTERVAL_SEC);
+    CHECK(timerfd, "create_timer");
+
+    // Add all fds to epoll set
+    struct epoll_event ev;
+
+    ev.data.fd = 0;
+    ev.events = EPOLLIN;
+    if (epoll_ctl(epoll_fd, EPOLL_CTL_ADD, 0, &ev) == 0)
+        log_info("Added stdin to epoll set");
+    else
+        die("epoll_ctl");
+
+    ev.data.fd = timerfd;
+    ev.events = EPOLLIN;
+    if (epoll_ctl(epoll_fd, EPOLL_CTL_ADD, timerfd, &ev) == 0)
+        log_info("Added timer (%d) to epoll set", timerfd);
+    else
+        die("epoll_ctl");
+
+    ev.data.fd = g_client.sock;
+    ev.events = EPOLLIN | EPOLLOUT;
+    if (epoll_ctl(epoll_fd, EPOLL_CTL_ADD, g_client.sock, &ev) == 0)
+        log_info("Added socket (%d) to epoll set", g_client.sock);
+    else
+        die("epoll_ctl");
+
+    char read_buf[MAX_MSG_LEN + 1];  // buffer to store bytes being read from the server
+    char write_buf[MAX_MSG_LEN + 1]; // buffer to store bytes being sent to the server
+
+    size_t read_buf_len = 0;  // size of the read buffer
+    size_t write_buf_len = 0; // size of the write buffer
+    size_t write_buf_off = 0; // position in the write buffer
+
+    // Array to store the returned events from epoll_wait
+    struct epoll_event events[5];
+
+    // Register signal handler to quit on CTRL-C
+    signal(SIGINT, signal_handler);
+
+    // Register the client in IRC network
+#ifdef DEBUG
+    // strcpy(g_client.nick, "aaryab2");
+    strcpy(g_client.username, "aarya.bhatia1678");
+    strcpy(g_client.realname, "Aarya Bhatia");
+// #else
+//     sprintf(buffer, "Enter your nick > ");
+//     write(1, buffer, strlen(buffer));
+//     scanf("%s", g_client.nick);
+
+//     sprintf(buffer, "Enter your username > ");
+//     write(1, buffer, strlen(buffer));
+//     scanf("%s", g_client.username);
+
+//     sprintf(buffer, "Enter your realname > ");
+//     write(1, buffer, strlen(buffer));
+//     scanf("%s", g_client.realname);
+#endif
+
+    char *initial_requests = NULL;
+    asprintf(&initial_requests, "NICK %s\r\nUSER %s * * :%s\r\n", g_client.nick, g_client.username, g_client.realname);
+    cc_list_add_last(g_queue, initial_requests);
+
+    while (running)
+    {
+        // Poll event set
+        int nfd = epoll_wait(epoll_fd, events, MAX_EPOLL_EVENTS, EPOLL_TIMEOUT);
+
+        if (nfd == -1)
+        {
+            perror("epoll_wait");
+            exit(1);
+        }
+        if (nfd == 0)
+        {
+            log_debug("No events occurred");
+            continue;
+        }
+
+        // Check for events
+        for (size_t i = 0; i < MAX_EPOLL_EVENTS; i++)
+        {
+            int fd = events[i].data.fd;
+            int event = events[i].events;
+
+            // stdin event
+            if (fd == 0 && event & EPOLLIN)
+            {
+                // Read the next line till \n
+                read_user_input();
+                cc_list_add_last(g_queue, strdup(g_line));
+            }
+
+            // timer event
+            else if (fd == timerfd && event & EPOLLIN)
+            {
+                uint64_t t = 0;
+
+                // Timer expired
+                if (read(timerfd, &t, 8) > 0)
+                {
+                    log_debug("Timer expired at %d", (int)time(NULL));
+
+                    // Only ping if idle
+                    if (write_buf_len > 0)
+                    {
+                        continue;
+                    }
+
+                    // Check time since last message sent
+                    if (difftime(time(NULL), g_last_request) > PING_INTERVAL_SEC)
+                    {
+                        // Make ping request
+                        cc_list_add_last(g_queue, strdup("PING\r\n"));
+                        sleep(1);
+                    }
+                }
+            }
+
+            // socket event
+            else if (fd == g_client.sock)
+            {
+                if (event & (EPOLLERR | EPOLLHUP))
+                {
+                    die(NULL);
+                }
+
+                // socket is ready to send data i.e. client can read
+                if (event & EPOLLIN)
+                {
+                    read_from_socket(g_client.sock, read_buf, &read_buf_len);
+                }
+
+                // socket is ready to recieve data i.e. client can write data
+                if (event & EPOLLOUT)
+                {
+                    write_to_socket(g_client.sock, write_buf, &write_buf_len, &write_buf_off);
+                }
+            }
+        }
+    }
+
+    quit();
+
+    return 0;
+}
 
 void read_user_input()
 {
@@ -150,159 +326,4 @@ int create_timer(int duration_sec)
         die("timerfd_settime");
 
     return fd;
-}
-
-int main(int argc, char *argv[])
-{
-    if (argc != 3)
-    {
-        fprintf(stderr, "Usage: %s <hostname> <port>\n", *argv);
-        return 1;
-    }
-
-    Client_init(&g_client);
-    Client_connect(&g_client, argv[1], argv[2]);
-
-    g_last_request = time(NULL);
-
-    // Create fd for epoll
-    int epoll_fd = epoll_create1(0);
-
-    // Create a timer fd to send PING messages
-    int timerfd = create_timer(PING_INTERVAL_SEC);
-
-    // Add all fds to epoll set
-
-    struct epoll_event ev;
-
-    ev.data.fd = 0;
-    ev.events = EPOLLIN;
-    epoll_ctl(epoll_fd, EPOLL_CTL_ADD, 0, &ev);
-
-    ev.data.fd = timerfd;
-    ev.events = EPOLLIN;
-    epoll_ctl(epoll_fd, EPOLL_CTL_ADD, timerfd, &ev);
-
-    ev.data.fd = g_client.sock;
-    ev.events = EPOLLIN | EPOLLOUT;
-    epoll_ctl(epoll_fd, EPOLL_CTL_ADD, g_client.sock, &ev);
-
-    char read_buf[MAX_MSG_LEN + 1];  // buffer to store bytes being read from the server
-    char write_buf[MAX_MSG_LEN + 1]; // buffer to store bytes being sent to the server
-
-    size_t read_buf_len = 0;  // size of the read buffer
-    size_t write_buf_len = 0; // size of the write buffer
-    size_t write_buf_off = 0; // position in the write buffer
-
-    // Array to store the returned events from epoll_wait
-    struct epoll_event events[5];
-
-    // Register signal handler to quit on CTRL-C
-    signal(SIGINT, signal_handler);
-
-    // Register the client in IRC network
-#ifdef DEBUG
-    // strcpy(g_client.nick, "aaryab2");
-    strcpy(g_client.username, "aarya.bhatia1678");
-    strcpy(g_client.realname, "Aarya Bhatia");
-// #else
-//     sprintf(buffer, "Enter your nick > ");
-//     write(1, buffer, strlen(buffer));
-//     scanf("%s", g_client.nick);
-
-//     sprintf(buffer, "Enter your username > ");
-//     write(1, buffer, strlen(buffer));
-//     scanf("%s", g_client.username);
-
-//     sprintf(buffer, "Enter your realname > ");
-//     write(1, buffer, strlen(buffer));
-//     scanf("%s", g_client.realname);
-#endif
-
-    char *initial_requests;
-    asprintf(&initial_requests, "NICK %s\r\nUSER %s * * :%s\r\n", g_client.nick, g_client.username, g_client.realname);
-    cc_list_add_last(g_queue, initial_requests);
-
-    while (running)
-    {
-        // Poll event set
-        int nfd = epoll_wait(epoll_fd, events, 5, EPOLL_TIMEOUT);
-
-        if (nfd == -1)
-        {
-            perror("epoll_wait");
-            exit(1);
-        }
-        if (nfd == 0)
-        {
-            log_debug("No events occurred");
-            continue;
-        }
-
-        // Check for events
-        for (size_t i = 0; i < 5; i++)
-        {
-            int fd = events[i].data.fd;
-            int event = events[i].events;
-
-            // stdin event
-            if (fd == 0 && event & EPOLLIN)
-            {
-                // Read the next line till \n
-                read_user_input();
-                cc_list_add_last(g_queue, strdup(g_line));
-            }
-
-            // timer event
-            else if (fd == timerfd && event & EPOLLIN)
-            {
-                uint64_t t = 0;
-
-                // Timer expired
-                if (read(timerfd, &t, 8) > 0)
-                {
-                    log_debug("Timer expired at %d", (int)time(NULL));
-
-                    // Only ping if idle
-                    if (write_buf_len > 0)
-                    {
-                        continue;
-                    }
-
-                    // Check time since last message sent
-                    if (difftime(time(NULL), g_last_request) > PING_INTERVAL_SEC)
-                    {
-                        // Make ping request
-                        cc_list_add_last(g_queue, strdup("PING\r\n"));
-                        sleep(1);
-                    }
-                }
-            }
-
-            // socket event
-            else if (fd == g_client.sock)
-            {
-                if (event & (EPOLLERR | EPOLLHUP))
-                {
-                    die(NULL);
-                }
-
-                // socket is ready to send data i.e. client can read
-                if (event & EPOLLIN)
-                {
-                    read_from_socket(g_client.sock, read_buf, &read_buf_len);
-                }
-
-                // socket is ready to recieve data i.e. client can write data
-                if (event & EPOLLOUT)
-                {
-                    write_to_socket(g_client.sock, write_buf, &write_buf_len, &write_buf_off);
-                }
-            }
-        }
-    }
-
-    quit();
-
-    return 0;
 }
