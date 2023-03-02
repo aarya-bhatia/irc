15e7610d615649b3f91a4104bd263ed3368df179
Aarya Bhatia <aarya.bhatia1678@gmail.com>
2023-03-02 15:30:54 -0600

    indented code

--- src/register.c
+++ src/register.c
@@ -6,134 +6,140 @@
  * To complete registration, the user must have a username, realname and a nick.
  * If user has not set a nick, it is possible to use a previous nick if there is one and complete registration.
  */
-bool check_registration_complete(Server *serv, User *usr)
+bool check_registration_complete(Server * serv, User * usr)
 {
-    if (usr->registered)
-    {
-        return true;
-    }
+	if (usr->registered) {
+		return true;
+	}
 
-    if (!usr->nick || !usr->username || !usr->realname)
-    {
-        return false;
-    }
+	if (!usr->nick || !usr->username || !usr->realname) {
+		return false;
+	}
 
-    if (!usr->nick_changed)
-    {
-        CC_Array *nicks = get_current_nicks(serv, usr);
+	if (!usr->nick_changed) {
+		CC_Array *nicks = get_current_nicks(serv, usr);
 
-        if (nicks && cc_array_size(nicks) > 0)
-        {
-            char *nick = NULL;
+		if (nicks && cc_array_size(nicks) > 0) {
+			char *nick = NULL;
 
-            if (cc_array_get_at(nicks, 0, (void **)&nick) == CC_OK)
-            {
-                usr->nick_changed = true;
-                usr->nick = strdup(nick);
-            }
-        }
-    }
+			if (cc_array_get_at(nicks, 0, (void **)&nick) == CC_OK) {
+				usr->nick_changed = true;
+				usr->nick = strdup(nick);
+			}
+		}
+	}
 
-    if (usr->nick_changed)
-    {
-        usr->registered = true;
+	if (usr->nick_changed) {
+		usr->registered = true;
 
-        User_add_msg(usr, make_reply(":%s " RPL_WELCOME_MSG, serv->hostname, usr->nick, usr->nick));
-        User_add_msg(usr, make_reply(":%s " RPL_YOURHOST_MSG, serv->hostname, usr->nick, usr->hostname));
-        User_add_msg(usr, make_reply(":%s " RPL_CREATED_MSG, serv->hostname, usr->nick, serv->created_at));
-        User_add_msg(usr, make_reply(":%s " RPL_MYINFO_MSG, serv->hostname, usr->nick, serv->hostname, "*", "*", "*"));
+		User_add_msg(usr,
+			     make_reply(":%s " RPL_WELCOME_MSG, serv->hostname,
+					usr->nick, usr->nick));
+		User_add_msg(usr,
+			     make_reply(":%s " RPL_YOURHOST_MSG, serv->hostname,
+					usr->nick, usr->hostname));
+		User_add_msg(usr,
+			     make_reply(":%s " RPL_CREATED_MSG, serv->hostname,
+					usr->nick, serv->created_at));
+		User_add_msg(usr,
+			     make_reply(":%s " RPL_MYINFO_MSG, serv->hostname,
+					usr->nick, serv->hostname, "*", "*",
+					"*"));
 
-        char *motd = serv->motd_file ? get_motd(serv->motd_file) : NULL;
+		char *motd = serv->motd_file ? get_motd(serv->motd_file) : NULL;
 
-        if (motd)
-        {
-            User_add_msg(usr, make_reply(":%s " RPL_MOTD_MSG, serv->hostname, usr->nick, motd));
-        }
-        else
-        {
-            User_add_msg(usr, make_reply(":%s " ERR_NOMOTD_MSG, serv->hostname, usr->nick));
-        }
+		if (motd) {
+			User_add_msg(usr,
+				     make_reply(":%s " RPL_MOTD_MSG,
+						serv->hostname, usr->nick,
+						motd));
+		} else {
+			User_add_msg(usr,
+				     make_reply(":%s " ERR_NOMOTD_MSG,
+						serv->hostname, usr->nick));
+		}
 
-        update_nick_map(serv, usr);
+		update_nick_map(serv, usr);
 
-        log_info("registration completed for user %s", usr->nick);
+		log_info("registration completed for user %s", usr->nick);
 
-        return true;
-    }
+		return true;
+	}
 
-    return false;
+	return false;
 }
 
 /**
  * Check if user's nick needs to be recorded in nick_map and do the update.
  * Returns true if update was successful or false if there was any error.
  */
-bool update_nick_map(Server *serv, User *usr)
+bool update_nick_map(Server * serv, User * usr)
 {
-    assert(serv);
-    assert(usr);
+	assert(serv);
+	assert(usr);
 
-    if (!usr->registered)
-    {
-        log_warn("user %s has not registered", usr->nick);
-        return false;
-    }
+	if (!usr->registered) {
+		log_warn("user %s has not registered", usr->nick);
+		return false;
+	}
 
-    assert(usr->nick);
-    assert(usr->username);
-    assert(usr->realname);
+	assert(usr->nick);
+	assert(usr->username);
+	assert(usr->realname);
 
-    if (!cc_hashtable_contains_key(serv->user_to_nicks_map, usr->username))
-    {
-        log_debug("Adding user %s to nick_map", usr->nick);
-        CC_Array *arr = NULL;
-        cc_array_new(&arr);
-        cc_array_add(arr, strdup(usr->nick));
-        cc_hashtable_add(serv->user_to_nicks_map, strdup(usr->username), arr);
-        return true;
-    }
+	if (!cc_hashtable_contains_key(serv->user_to_nicks_map, usr->username)) {
+		log_debug("Adding user %s to nick_map", usr->nick);
+		CC_Array *arr = NULL;
+		cc_array_new(&arr);
+		cc_array_add(arr, strdup(usr->nick));
+		cc_hashtable_add(serv->user_to_nicks_map, strdup(usr->username),
+				 arr);
+		return true;
+	}
 
-    CC_Array *nicks = NULL;
+	CC_Array *nicks = NULL;
 
-    if (cc_hashtable_get(serv->user_to_nicks_map, usr->username, (void **)&nicks) == CC_OK)
-    {
-        // Check if nick already exists in array
-        for (size_t i = 0; i < cc_array_size(nicks); i++)
-        {
-            char *value = NULL;
+	if (cc_hashtable_get
+	    (serv->user_to_nicks_map, usr->username,
+	     (void **)&nicks) == CC_OK) {
+		// Check if nick already exists in array
+		for (size_t i = 0; i < cc_array_size(nicks); i++) {
+			char *value = NULL;
 
-            if (cc_array_get_at(nicks, i, (void **)&value) == CC_OK && !strcmp(value, usr->nick))
-            {
-                return true;
-            }
-        }
+			if (cc_array_get_at(nicks, i, (void **)&value) == CC_OK
+			    && !strcmp(value, usr->nick)) {
+				return true;
+			}
+		}
 
-        log_info("Adding user %s to nick_map", usr->nick);
-        cc_array_add(nicks, strdup(usr->nick));
-        return true;
-    }
+		log_info("Adding user %s to nick_map", usr->nick);
+		cc_array_add(nicks, strdup(usr->nick));
+		return true;
+	}
 
-    return false;
+	return false;
 }
 
 /**
  * Get current nicks for given user.
  */
-CC_Array *get_current_nicks(Server *serv, User *usr)
+CC_Array *get_current_nicks(Server * serv, User * usr)
 {
-    if (!usr->username || !cc_hashtable_contains_key(serv->user_to_nicks_map, usr->username))
-    {
-        return NULL;
-    }
+	if (!usr->username
+	    || !cc_hashtable_contains_key(serv->user_to_nicks_map,
+					  usr->username)) {
+		return NULL;
+	}
 
-    CC_Array *nicks = NULL;
+	CC_Array *nicks = NULL;
 
-    if (cc_hashtable_get(serv->user_to_nicks_map, usr->username, (void **)&nicks) != CC_OK)
-    {
-        return NULL;
-    }
+	if (cc_hashtable_get
+	    (serv->user_to_nicks_map, usr->username,
+	     (void **)&nicks) != CC_OK) {
+		return NULL;
+	}
 
-    return nicks;
+	return nicks;
 }
 
 /**
@@ -141,45 +147,40 @@ CC_Array *get_current_nicks(Server *serv, User *usr)
  * A user can use a nick if no other user is using it now or has used it before,
  * or if the user has used same nick before.
  */
-bool check_nick_available(Server *serv, User *usr, char *nick)
+bool check_nick_available(Server * serv, User * usr, char *nick)
 {
-    if (usr->nick && !strcmp(usr->nick, nick))
-    {
-        // nick did not change
-        return true;
-    }
+	if (usr->nick && !strcmp(usr->nick, nick)) {
+		// nick did not change
+		return true;
+	}
 
-    CC_HashTableIter iter;
-    cc_hashtable_iter_init(&iter, serv->user_to_nicks_map);
+	CC_HashTableIter iter;
+	cc_hashtable_iter_init(&iter, serv->user_to_nicks_map);
 
-    TableEntry *entry = NULL;
+	TableEntry *entry = NULL;
 
-    // Find nick
-    while (cc_hashtable_iter_next(&iter, &entry) != CC_ITER_END)
-    {
-        CC_Array *nicks = entry->value;
+	// Find nick
+	while (cc_hashtable_iter_next(&iter, &entry) != CC_ITER_END) {
+		CC_Array *nicks = entry->value;
 
-        for (size_t i = 0; i < cc_array_size(nicks); i++)
-        {
-            char *found = NULL;
+		for (size_t i = 0; i < cc_array_size(nicks); i++) {
+			char *found = NULL;
 
-            if (cc_array_get_at(nicks, i, (void **)&found) == CC_OK)
-            {
-                if (!strcmp(found, nick))
-                {
-                    // Check if username belongs to current user
-                    if (usr->username && !strcmp(entry->key, usr->username))
-                    {
-                        return true;
-                    }
+			if (cc_array_get_at(nicks, i, (void **)&found) == CC_OK) {
+				if (!strcmp(found, nick)) {
+					// Check if username belongs to current user
+					if (usr->username
+					    && !strcmp(entry->key,
+						       usr->username)) {
+						return true;
+					}
+					// nick is taken by another user
+					return false;
+				}
+			}
+		}
+	}
 
-                    // nick is taken by another user
-                    return false;
-                }
-            }
-        }
-    }
-
-    // nick not used by any user
-    return true;
+	// nick not used by any user
+	return true;
 }
--- src/server_main.c
+++ src/server_main.c
@@ -9,110 +9,101 @@ void sighandler(int sig);
  */
 int main(int argc, char *argv[])
 {
-    if (argc != 2)
-    {
-        fprintf(stderr, "Usage: %s <port>", *argv);
-        return 1;
-    }
+	if (argc != 2) {
+		fprintf(stderr, "Usage: %s <port>", *argv);
+		return 1;
+	}
+	// Create and start an IRC server on given port
+	Server *serv = Server_create(atoi(argv[1]));
 
-    // Create and start an IRC server on given port
-    Server *serv = Server_create(atoi(argv[1]));
+	// Array for events returned from epoll
+	struct epoll_event events[MAX_EVENTS] = { 0 };
 
-    // Array for events returned from epoll
-    struct epoll_event events[MAX_EVENTS] = {0};
+	// Event for listener socket
+	struct epoll_event ev = {.events = EPOLLIN,.data.fd = serv->fd };
 
-    // Event for listener socket
-    struct epoll_event ev = {.events = EPOLLIN, .data.fd = serv->fd};
+	CHECK(epoll_ctl(serv->epollfd, EPOLL_CTL_ADD, serv->fd, &ev),
+	      "epoll_ctl");
 
-    CHECK(epoll_ctl(serv->epollfd, EPOLL_CTL_ADD, serv->fd, &ev), "epoll_ctl");
+	// Setup signal handler to stop server
+	struct sigaction sa;
+	sigemptyset(&sa.sa_mask);
+	sa.sa_handler = sighandler;
+	sa.sa_flags = SA_RESTART;
 
-    // Setup signal handler to stop server
-    struct sigaction sa;
-    sigemptyset(&sa.sa_mask);
-    sa.sa_handler = sighandler;
-    sa.sa_flags = SA_RESTART;
+	if (sigaction(SIGINT, &sa, NULL) == -1)
+		die("sigaction");
 
-    if (sigaction(SIGINT, &sa, NULL) == -1)
-        die("sigaction");
+	if (sigaction(SIGPIPE, &sa, NULL) == -1)
+		die("sigaction");
 
-    if (sigaction(SIGPIPE, &sa, NULL) == -1)
-        die("sigaction");
+	// Run while g_alive flag is set
+	while (g_alive) {
+		int num = epoll_wait(serv->epollfd, events, MAX_EVENTS, -1);
 
-    // Run while g_alive flag is set
-    while (g_alive)
-    {
-        int num = epoll_wait(serv->epollfd, events, MAX_EVENTS, -1);
+		if (num == -1) {
+			perror("epoll_wait");
+			g_alive = false;
+			break;
+		}
 
-        if(num == -1){
-            perror("epoll_wait");
-            g_alive = false;
-            break;
-        }
+		for (int i = 0; i < num; i++) {
+			// Event on listening socket
+			if (events[i].data.fd == serv->fd) {
+				// Accept all new connections
+				Server_accept_all(serv);
+			} else {
+				int e = events[i].events;
+				int fd = events[i].data.fd;
 
-        for (int i = 0; i < num; i++)
-        {
-            // Event on listening socket
-            if (events[i].data.fd == serv->fd)
-            {
-                // Accept all new connections
-                Server_accept_all(serv);
-            }
-            else
-            {
-                int e = events[i].events;
-                int fd = events[i].data.fd;
+				User *usr;
 
-                User *usr;
+				// Fetch user data from hashtable
+				if (cc_hashtable_get
+				    (serv->connections, (void *)&fd,
+				     (void **)&usr) != CC_OK) {
+					epoll_ctl(serv->epollfd, EPOLL_CTL_DEL,
+						  fd, NULL);
+					continue;
+				}
 
-                // Fetch user data from hashtable
-                if (cc_hashtable_get(serv->connections, (void *)&fd, (void **)&usr) != CC_OK)
-                {
-                    epoll_ctl(serv->epollfd, EPOLL_CTL_DEL, fd, NULL);
-                    continue;
-                }
+				if (e & (EPOLLERR | EPOLLHUP | EPOLLRDHUP)) {
+					log_debug("user %s disconnected",
+						  usr->nick);
+					User_Disconnect(serv, usr);
+					continue;
+				}
 
-                if (e & (EPOLLERR | EPOLLHUP | EPOLLRDHUP))
-                {
-                    log_debug("user %s disconnected", usr->nick);
-                    User_Disconnect(serv, usr);
-                    continue;
-                }
+				if (e & EPOLLIN) {
+					if (User_Read_Event(serv, usr) == -1) {
+						continue;
+					}
+				}
 
-                if (e & EPOLLIN)
-                {
-                    if (User_Read_Event(serv, usr) == -1)
-                    {
-                        continue;
-                    }
-                }
+				if (e & EPOLLOUT) {
+					if (User_Write_Event(serv, usr) == -1) {
+						continue;
+					}
+					// User is quitting and all pending messages were sent
+					if (usr->quit
+					    && cc_list_size(usr->msg_queue) == 0
+					    && usr->res_len == 0) {
+						User_Disconnect(serv, usr);
+						continue;
+					}
+				}
+			}
+		}
+	}
 
-                if (e & EPOLLOUT)
-                {
-                    if (User_Write_Event(serv, usr) == -1)
-                    {
-                        continue;
-                    }
+	Server_destroy(serv);
 
-                    // User is quitting and all pending messages were sent
-                    if (usr->quit && cc_list_size(usr->msg_queue) == 0 && usr->res_len == 0)
-                    {
-                        User_Disconnect(serv, usr);
-                        continue;
-                    }
-                }
-            }
-        }
-    }
-
-    Server_destroy(serv);
-
-    return 0;
+	return 0;
 }
 
 void sighandler(int sig)
 {
-    if (sig == SIGINT)
-    {
-        g_alive = false;
-    }
+	if (sig == SIGINT) {
+		g_alive = false;
+	}
 }
